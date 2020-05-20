86feffe86e9ff27f23de504a4d13bb977194c26a
Sungbin <ysb05222@naver.com>
2020-05-21 02:02:46 +0900

    Update UpdateUserEx.java

--- src/main/java/kr/ac/mjc/sungbin/spring/day2/class06/user/UpdateUserEx.java
+++ src/main/java/kr/ac/mjc/sungbin/spring/day2/class06/user/UpdateUserEx.java
@@ -1,22 +1,33 @@
 package kr.ac.mjc.sungbin.spring.day2.class06.user;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.DuplicateKeyException;
 
 public class UpdateUserEx {
 
-	public static void main(String[] args) {
-		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("applicationContext_User.xml");
-		UserDao userDao = (UserDao) context.getBean("userDao");
-		User user = new User();
-		user.setId("188");
-		user.setEmail("sungbin3763@inflearn.com");
-		user.setName("양성빈");
-		userDao.updateUser(user);
+	static final Logger logger = LogManager.getLogger();
 
-		user = userDao.getUser("188");
-		System.out.println("수정했습니다.\n" + user);
+	public static void main(String[] args) {
+		User user = new User();
+		user.setId("212");
+		user.setEmail("didtjdqls1223@inflearn.com");
+		user.setName("YangSungBin");
+
+		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(
+				"applicationContext_User.xml");
+		UserDao userDao = context.getBean("userDao", UserDao.class);
+
+		try {
+			userDao.updateUser(user);
+			logger.info("회원정보를 수정했습니다. {}", user);
+		} catch (DuplicateKeyException e) {
+			logger.debug("이메일이 중복되어 회원정보를 수정할 수 없습니다. email={}",
+					user.getEmail());
+		}
+
 		context.close();
 	}
-
 }
