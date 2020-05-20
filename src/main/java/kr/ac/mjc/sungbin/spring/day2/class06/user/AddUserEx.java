7b35447cdcdce7d76daa404943ffd547daaee5f5
Sungbin <ysb05222@naver.com>
2020-05-21 02:00:30 +0900

    Update AddUserEx.java

--- src/main/java/kr/ac/mjc/sungbin/spring/day2/class06/user/AddUserEx.java
+++ src/main/java/kr/ac/mjc/sungbin/spring/day2/class06/user/AddUserEx.java
@@ -1,26 +1,30 @@
 package kr.ac.mjc.sungbin.spring.day2.class06.user;
 
-import java.util.List;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.DuplicateKeyException;
 
 public class AddUserEx {
 
+	static final Logger logger = LogManager.getLogger();
+
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("applicationContext_User.xml");
-		UserDao userDao = (UserDao) context.getBean("userDao");
 		User user = new User();
-		user.setEmail("ysb0522222@maver.com");
+		user.setEmail("didtjdqls1223@inflearn.com");
 		user.setPassword("1234");
 		user.setName("양성빈");
-		userDao.addUser(user);
-		System.out.println("사용자를 추가했습니다.");
 
-		List<User> userList = userDao.listUsers(1, 10);
-		for (User u : userList)
-			System.out.println(u);
+		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(
+				"applicationContext_User.xml");
+		UserDao userDao = context.getBean("userDao", UserDao.class);
+		try {
+			userDao.addUser(user);
+			logger.info("등록했습니다. {}", user);
+		} catch (DuplicateKeyException e) {
+			logger.debug("이메일이 중복되어 등록하지 못했습니다. email={}", user.getEmail());
+		}
 		context.close();
 	}
-
 }
