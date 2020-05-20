fabb23f21faf4874671cc991525ddb275a5a56f1
Sungbin <ysb05222@naver.com>
2020-05-21 02:02:06 +0900

    Update ListUserEx.java

--- src/main/java/kr/ac/mjc/sungbin/spring/day2/class06/user/ListUserEx.java
+++ src/main/java/kr/ac/mjc/sungbin/spring/day2/class06/user/ListUserEx.java
@@ -2,17 +2,20 @@ package kr.ac.mjc.sungbin.spring.day2.class06.user;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ListUserEx {
+	
+	static final Logger logger = LogManager.getLogger();
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("applicationContext_User.xml");
-		UserDao userDao = (UserDao) context.getBean("userDao");
-		List<User> userList = userDao.listUsers(1, 10);
-		for (User user : userList)
-			System.out.println(user);
+		UserDao userDao = context.getBean("userDao", UserDao.class);
+		List<User> userList = userDao.listUsers(0, 5);
+		logger.debug(userList);
 		context.close();
 	}
 
