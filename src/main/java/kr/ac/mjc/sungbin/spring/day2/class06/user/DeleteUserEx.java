e9b022507fe78db8c08385c3bc495aadc6a6c075
Sungbin <ysb05222@naver.com>
2020-05-21 02:01:02 +0900

    Update DeleteUserEx.java

--- src/main/java/kr/ac/mjc/sungbin/spring/day2/class06/user/DeleteUserEx.java
+++ src/main/java/kr/ac/mjc/sungbin/spring/day2/class06/user/DeleteUserEx.java
@@ -1,20 +1,21 @@
 package kr.ac.mjc.sungbin.spring.day2.class06.user;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DeleteUserEx {
 
+	static final Logger logger = LogManager.getLogger();
+
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("applicationContext_User.xml");
-		UserDao userDao = (UserDao) context.getBean("userDao");
-		String id = "190";
-		int updatedRows = userDao.deleteUser(id);
-		if (updatedRows > 0)
-			System.out.println("삭제했습니다. id=" + id);
-		else
-			System.out.println("잘못된 id입니다. id=" + id);
+		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(
+				"applicationContext_User.xml");
+		UserDao userDao = context.getBean("userDao", UserDao.class);
+		String id = "201";
+		userDao.deleteUser(id);
+		logger.info("회원탈퇴했습니다. id={}", id);
 		context.close();
 	}
-
 }
